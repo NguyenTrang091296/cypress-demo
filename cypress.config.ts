99b1bd049472f7bc187f45283c5f0d8d5ce2626d
NguyenTrang091296 <nguyentrang091296@gmail.com>
2025-09-08 18:57:57 +0700

    update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,18 +3,27 @@ import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    supportFile: "cypress/support/e2e.ts",
+
+    // Setup Allure
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
     },
-    baseUrl: "https://www.saucedemo.com", // URL mặc định
-    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
-    supportFile: "cypress/support/e2e.ts",
+
     env: {
       API_KEY: process.env.API_KEY || "default-local-key",
+      allure: true,
     },
   },
-  env: {
-    allure: true,
+
+  reporter: "allure-cypress",
+  reporterOptions: {
+    resultsDir: "allure-results",
+    overwrite: true,
+    useCypressSteps: true,
+    useCypressLabels: true,
   },
 });
