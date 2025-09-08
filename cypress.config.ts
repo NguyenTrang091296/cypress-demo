d88bdd858f60cbea259a31dba4800b510611af2a
NguyenTrang091296 <nguyentrang091296@gmail.com>
2025-09-08 09:41:02 +0700

    fix run

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from "cypress";
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 export default defineConfig({
-  projectId: 'xwf64n',
   e2e: {
-    baseUrl: "https://www.saucedemo.com/",
-    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
-    supportFile: "cypress/support/e2e.ts",
     setupNodeEvents(on, config) {
+      // Kích hoạt Allure plugin
+      allureWriter(on, config);
+
+      // Luôn return config
       return config;
     },
+    baseUrl: "https://www.saucedemo.com", // URL mặc định
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    supportFile: "cypress/support/e2e.ts",
   },
-  reporter: "allure-cypress",
-  reporterOptions: {
-    resultsDir: "allure-results"
-  }
+  env: {
+    allure: true,
+  },
 });
